97560172f189d7ec3c06e68e0b883815c13883b4
GitHub Action <action@github.com>
2020-10-21 22:33:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/nbadal/psifilebug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nbadal/psifilebug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nbadal.psifilebug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nbadal.psifilebug.MyBundle
 
 class MyApplicationService {
 
